edfb5c354d52552b2838a1beb081daa6eebb9fe4
Matthew Wheatley <terrawheat@gmail.com>
2016-08-04 14:23:22 +0100

    Just discovered refreshenv :D

--- newbuild/windows.ps1
+++ newbuild/windows.ps1
@@ -2,8 +2,8 @@ Set-ExecutionPolicy RemoteSigned
 
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+refreshenv
 
 choco install -y git nodejs mongodb rabbitmq nuget.commandline visualstudiocode slack 7zip.install putty curl vim wget
 
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+refreshenv
