a423e54385c990f7ea5e0b4c46b9d67b5a89eac8
Matt Wheatley <terrawheat@gmail.com>
2016-08-11 14:15:04 +0100

    Install correct MongoDB package for a windows build

--- newbuild/windows.ps1
+++ newbuild/windows.ps1
@@ -4,6 +4,6 @@ iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
 refreshenv
 
-choco install -y git nodejs mongodb rabbitmq nuget.commandline visualstudiocode slack 7zip.install putty curl vim wget
+choco install -y git nodejs mongodb.portable rabbitmq nuget.commandline visualstudiocode slack 7zip.install putty curl vim wget
 
 refreshenv
